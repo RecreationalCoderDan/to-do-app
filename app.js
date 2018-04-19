667bff920c9e0233c02e671a07e3c2d2780cce74
RecreationalCoderDan <calibound0495psnn@yandex.com>
2018-04-19 16:35:23 -0700

    added js functions still need delete button

---
+++ app.js
@@ -0,0 +1,56 @@
+ function onReady() {
+
+     const addToForm = document.getElementById('addToDoForm')
+     
+       addToDoForm.addEventListener();
+  addToDoForm.addEventListener('submit', () Event => {
+        event.preventDefault();
+     
+      // get the text
+    let title = newToDoText.value;
+     
+      // create a new li
+    let newLi = document.createElement('li');
+
+    // create a new input
+    let checkbox = document.createElement('input');
+
+    // set the input's type to checkbox
+    checkbox.type = "checkbox";
+     
+        // set the title
+    newLi.textContent = title;
+
+    // attach the checkbox to the li
+    newLi.appendChild(checkbox);
+
+      // attach the li to the ul
+    toDoList.appendChild(newLi);
+     
+     
+      //empty the input
+    newToDoText.value = '';
+     
+     
+
+  });
+}
+     const newtodotext = document.getElementById('newtodotext')
+     const toDoList = document.getElementById('toDoList')
+     
+     
+ }
+
+
+
+window.onload = function() {
+   alert("The window has loaded!");
+ };
+
+ onReady();
+
+
+
+
+
+ 
